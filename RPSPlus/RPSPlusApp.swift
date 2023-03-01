fd618a88aac42aa718c837a4044979a5067b8fa9
Brandon Coston <costonb@gmail.com>
2023-02-28 22:39:39 -0800

    Initial Commit

---
+++ RPSPlus/RPSPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSPlusApp.swift
+//  RPSPlus
+//
+//  Created by Brandon Coston on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct RPSPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
